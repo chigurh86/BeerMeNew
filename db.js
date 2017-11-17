c902e75e361078b4f58c716225fd520e632078d6
Antonio Caos Jr <antoniocaosjr.@Antonios-MacBook-Pro.local>
2017-11-16 19:29:07 -0500

    added comment

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
 })
 
 connection.connect()
-
+//comment
 // connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
 //   if (error) throw error;
 //   console.log('The solution is: ', results[0].solution);
