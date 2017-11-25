077071d1a524a100b629481eeacd59f6081226d4
Antonio Caos Jr <antoniocaosjr.@Antonios-MacBook-Pro.local>
2017-11-25 14:00:08 -0500

    added conditional to db file

--- db.js
+++ db.js
@@ -1,11 +1,17 @@
 var mysql = require('mysql')
-var connection = mysql.createConnection({
-  host: process.env.DB_HOST || "127.0.0.1" || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "root",
-  database : process.env.DB_NAME || "express-cc",
-  socketName: '/tmp/mysql.sock'
-})
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else{
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST || "127.0.0.1" || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "root",
+    database : process.env.DB_NAME || "express-cc",
+    socketName: '/tmp/mysql.sock'
+  });
+};
 
 connection.connect()
 //comment
