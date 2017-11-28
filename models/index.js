51540f548783d273affb20011491033b8da05bb8
Antonio Caos Jr <antoniocaosjr.@Antonios-MacBook-Pro.local>
2017-11-27 22:47:52 -0500

    fixed sequelize connection

--- models/index.js
+++ models/index.js
@@ -1,64 +1,70 @@
-'use strict';
+const fs = require('fs');
+const path = require('path');
+const mysql = require('mysql');
+const Sequelize = require('sequelize');
+const winston = require('winston');
 
- var fs = require('fs');
- var path = require('path');
- const Sequelize = require('sequelize');
+const basename = path.basename(module.filename);
+const env = process.env.NODE_ENV || 'development';
 
- var orm = {};
+const db = {};
 
-console.log('----------------> i got here', process.env.NODE_ENV, process.env.JAWSDB_URL);
+const database = process.env.DB_NAME || 'express-cc';
+const username = process.env.DB_USER || 'root';
+const password = process.env.DB_PASSWORD || 'root';
+const host = process.env.DB_HOST || 'localhost';
 
- var dbName = process.env.DB_NAME || 'express-cc';
- var dbUser = process.env.DB_USER || 'root';
- var dbPassword = process.env.DB_PASSWORD || 'root';
- var dbHost = process.env.DB_HOST || 'localhost';
+let dbConnection;
+if (env === 'production') {
+  dbConnection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  dbConnection = mysql.createConnection({
+    host,
+    user: username,
+    password,
+    database,
+    socketName: '/tmp/mysql.sock'
+  });
+}
 
- /*const sequelize = new Sequelize(prcoess, "root", "root", {
-   host: 'localhost',
-   dialect: 'mysql',
-   pool: {
-     max: 5,
-     min: 0,
-     idle: 10000
-   }, define: {
-        timestamps: false
+let sequelize;
+
+if (env === 'production') {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(database, username, password, {
+    host,
+    dialect: 'mysql',
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    },
+    define: {
+      timestamps: false
     }
 
- });
-*/
- const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
-   host: dbHost,
-   dialect: 'mysql',
-   pool: {
-     max: 5,
-     min: 0,
-     idle: 10000
-   }, define: {
-        timestamps: false
-    }
+  });
+}
 
- });
+fs
+  .readdirSync(__dirname)
+  .filter(file =>
+    (file.indexOf('.') !== 0) &&
+    (file !== basename) &&
+    (file.slice(-3) === '.js'))
+  .forEach(file => {
+    const model = sequelize.import(path.join(__dirname, file));
+    db[model.name] = model;
+  });
 
- fs
-   .readdirSync(__dirname)
-   .filter(function (file) {
-     return (file.indexOf('.') !== 0) && (file !== 'index.js');
-   })
-   .forEach(function (file) {
-     var model = sequelize.import(path.join(__dirname, file));
-     orm[model.name] = model;
-   });
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
 
- Object.keys(orm).forEach(function (modelName) {
-   if ('associate' in orm[modelName]) {
-     orm[modelName].associate(orm);
-   }
- });
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
- // Create tables for all models that don't
- // currently have a table in the current database.
- sequelize.sync();
- orm.sequelize = sequelize;
- orm.Sequelize = Sequelize;
-
- module.exports = orm;
+module.exports = db;
